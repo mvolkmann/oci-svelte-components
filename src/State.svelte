2ebb8daee0a3791f27270dc7b27c061aefa6f769
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-05-21 14:08:50 -0500

    added comments in State.svelte

--- src/State.svelte
+++ src/State.svelte
@@ -1,4 +1,24 @@
 <script>
+  // This is an interesting approach to handling the
+  // association between a component and global state.
+  // For example, the following renders an
+  // instance of the LabeledInput component.
+  //
+  // <State
+  //   component={LabeledInput}
+  //   label="Middle Name"
+  //   path="user.profile.middleName" />
+  //
+  // You can pass any props to this component.
+  // The props "component" and "path" are only used by this component.
+  // All other props are passed on to the specified component.
+  // This uses the "path" prop to retrieve a value from $globalStore.
+  // That value is passed to the specified component.
+  // The specified component is expected to dispatch a "value" event
+  // when the user changes the value.
+  // This component handles the "value" event by updating
+  // the value in $globalStore at the specified path.
+
   import get from 'lodash-es/get';
   import {globalStore, update} from './stores';
 
