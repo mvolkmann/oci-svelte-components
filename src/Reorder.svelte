e2608dd3f887dcbe3b89c8ab1d7b1e739cf4f2ab
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-05-29 16:25:45 -0500

    progress on Reorder

--- src/Reorder.svelte
+++ src/Reorder.svelte
@@ -1,57 +1,99 @@
 <script>
+  import {quintOut} from 'svelte/easing';
+  import {crossfade} from 'svelte/transition';
+  import {flip} from 'svelte/animate';
+
   export let items;
 
-  let dragElement;
-  let parentRef;
+  let container;
+  let dragContainer;
+  let dragItem;
+  let offsetX;
+  let offsetY;
 
-  function dragOver(event) {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = 'move';
+  const [send, receive] = crossfade({});
+
+  function mouseDown(event) {
+    const dragElement = event.target;
+    dragItem = dragElement.textContent.trim();
+    items = items.filter(item => item !== dragItem);
+
+    const rect = dragElement.getBoundingClientRect();
+    offsetX = event.x - rect.x;
+    offsetX = event.y - rect.y;
+
+    dragContainer.firstChild.textContent = dragItem;
+    dragContainer.style.left = rect.x + 'px';
+    dragContainer.style.top = rect.y + 'px';
+    dragContainer.style.visibility = 'visible';
   }
 
-  function dragStart(event) {
-    dragElement = event.target;
-    //const text = event.target.textContent;
-    //event.dataTransfer.setData('text/plain', text);
-    event.dataTransfer.dropEffect = 'move';
+  function mouseMove(event) {
+    if (!dragItem) return;
+
+    dragContainer.style.left = event.x - offsetX + 'px';
+    dragContainer.style.top = event.y - offsetY + 'px';
   }
 
-  function drop(event) {
-    event.preventDefault();
-    const {clientX} = event;
-
+  function mouseUp(event) {
     // Find child before the drop location.
-    const children = parentRef.children;
-    const nextSibling = Array.from(children).find(
+    const {clientX} = event;
+    const index = Array.from(container.children).findIndex(
       child => clientX < child.getBoundingClientRect().x
     );
 
-    parentRef.insertBefore(dragElement, nextSibling);
+    // Insert the item being dragged back into items.
+    if (index === -1) {
+      items.push(dragItem);
+    } else {
+      items.splice(index, 0, dragItem);
+    }
+    items = items; // trigger reactivity
+
+    dragContainer.style.visibility = 'hidden';
+    dragItem = null;
   }
 </script>
 
-<div
-  bind:this={parentRef}
-  class="osc-reorder"
-  on:dragover={dragOver}
-  on:drop={drop}>
-  {#each items as item}
-    <!-- Must set draggable to true, not just have the attribute! -->
-    <div class="item" draggable="true" on:dragstart={dragStart}>{item}</div>
-  {/each}
+<div class="osc-reorder">
+  <div bind:this={container} class="container">
+    {#each items as item (item)}
+      <div
+        animate:flip
+        class="item"
+        in:receive={{key: item}}
+        out:send={{key: item}}
+        on:mousedown={mouseDown}>
+        {item}
+      </div>
+    {/each}
+  </div>
+
+  <div bind:this={dragContainer} class="drag-container">
+    <div class="item" on:mousemove={mouseMove} on:mouseup={mouseUp}>
+      {dragItem}
+    </div>
+  </div>
 </div>
 
 <style>
-  .item {
-    display: inline-block;
-    border: solid blue 3px;
-    cursor: move;
-    margin-right: 1rem;
+  .container {
+    border: solid red 2px;
+    display: flex;
     padding: 1rem;
   }
 
-  .osc-reorder {
-    border: solid red 2px;
+  .drag-container {
+    position: absolute;
+    visibility: hidden;
+  }
+
+  .item {
+    display: inline-block;
+    background-color: #cccccc80;
+    border: solid blue 3px;
+    cursor: move;
+    margin-right: 1rem;
     padding: 1rem;
   }
 </style>
