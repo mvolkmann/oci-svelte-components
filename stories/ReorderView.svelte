0c6cf980bb560945fd277237370699b0565fb34f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-05-29 16:59:59 -0500

    more on Reorder

--- stories/ReorderView.svelte
+++ stories/ReorderView.svelte
@@ -2,6 +2,7 @@
   import get from 'lodash-es/get';
   import Reorder from '../src/Reorder.svelte';
   import {globalStore} from '../src/stores';
+  import LabeledState from '../src/LabeledState.svelte';
   import Toggle from '../src/Toggle.svelte';
 
   globalStore.set({
@@ -10,15 +11,14 @@
     }
   });
 
-  $: console.log(
-    'ReorderView.svelte: colorPrefererences =',
-    $globalStore.user.colorPreferences
-  );
-
   const verticalPath = 'select.vertical';
 
   $: vertical = get($globalStore, verticalPath);
+
+  const label = 'Color Preferences';
+  const path = 'user.colorPreferences';
 </script>
 
 <Toggle label="Vertical" path={verticalPath} />
-<Reorder label="Color Preferences" path="user.colorPreferences" {vertical} />
+<Reorder {label} {path} {vertical} />
+<LabeledState {label} {path} />
